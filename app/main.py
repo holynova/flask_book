fb9b367b458a24c52432f8e954fdc5a5b1ea77cf
moonreader <holy_nova@126.com>
2016-06-16 17:47:33 +0800

    增加注册页面、写新文章界面

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
+import logging
+import sqlite3
+import random,os
 from flask import Flask,url_for,render_template,flash,request,session,redirect
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
-from wtforms import  BooleanField,StringField,SubmitField , TextField, PasswordField, validators
-import logging
+from wtforms import BooleanField,StringField,SubmitField,TextAreaField,TextField,PasswordField,validators
+
 
 # from wrforms.validators import Required
 # from wtforms.validators import Required
@@ -112,9 +115,87 @@ def reg_success():
 	return render_template('reg_success.html',username = session.get('username'),email = session.get('email'),password = session.get('password'),)
 	# pass
 
+class Article(Form):
+	title =  TextField(u'标题',[validators.Required(),validators.Length(min = 4,max=25)])
+	author =  TextField(u'作者',[validators.Required(),validators.Length(min = 4,max=25)])
+	content = TextAreaField(u'正文',[validators.Required()])
+	submit = SubmitField(u'发布')
+
+class DB:
+	def __init__(self,db_name='db.sql'):
+		if not os.path.exists(db_name):
+			print "new db ",db_name,' created'
+		self.conn = sqlite3.connect(db_name)
+		self.c = self.conn.cursor()
+		# self.creat_table()
+		# self.random_insert()
+	def creat_table(self,table_name='arts'):
+		order = 'DROP TABLE IF EXISTS '+table_name
+		self.c.execute(order)
+		order = 'CREATE TABLE '+table_name+' (title text,author text,content text)'
+		self.c.execute(order)
+
+	def random_insert(self,table_name='arts',N=5):
+		arts = []
+		for i in range(N):
+			art = (random.choice(u'个人日记 工作感悟 学习笔记 编程学习'.split())+str(i),
+				random.choice(u'Zhao Qian Sun Li'.split())+'_'+random.choice('San Si Mazi'.split()),
+				random.choice([u'我是正文.\n'*10,u'我是正文他爹.\n'*10,u'我是正文他爷爷.\n'*10,])
+				)
+			arts.append(art)
+		logging.error('%d articles added' %N)
+		order = 'INSERT INTO '+ table_name+' VALUES (?,?,?)'
+		self.c.executemany(order,arts)
+		self.conn.commit()
+
+	def insert_one(self,table_name,art_tuple):
+		order = 'INSERT INTO '+ table_name+' VALUES (?,?,?)'
+		self.c.execute(order,art_tuple)
+		self.conn.commit()
+		logging.error('a new line has been added to database.')
+
+	def find_all(self,table_name = 'arts'):
+		'''
+		找到整个数据库中存储的数据,并返回
+		返回值为一个obj, 但实际上是可以迭代的
+		'''
+		order = 'SELECT * FROM '+table_name
+		return self.c.execute(order)
+		
 
 
 
+
+@app.route('/new',methods = ['GET',"POST"])
+def new_art():
+	db = DB(db_name = 'db.sql')
+	db.creat_table(table_name = 'arts')
+	db.random_insert('arts',5)
+
+	art = Article(request.form)
+	if request.method == 'POST':
+		if art.validate():
+			logging.error('new art added')
+			#存入数据库
+			db.insert_one(table_name = 'arts',art_tuple = (art.title,art.author,art.content))
+
+			#生成一个唯一的文章ID
+			#跳转到单篇文章视图,显示预览
+
+#step1 先用单页面实现数据库的存储和显示问题
+#step2 改进成为有编辑页\文章单页\全部文章单独的页面的方式
+	arts = db.find_all('arts')
+	for art in arts:
+		print art
+		# logging.error(art)	
+
+	#arts = 数据库中所有文章
+	return render_template('new_art.html',form = art,arts = arts)
+
+
+@app.route('/articles/<art_id>')
+def show_all_arts(art_id):
+	pass
 
 
 
